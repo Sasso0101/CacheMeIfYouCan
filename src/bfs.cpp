e4827bd69523e0ba3eb6fc8731c777fcb48b9e78
sasso0101 <git@sasso.dev>
2025-01-13 10:52:11 +0100

    Changed to constexpr

--- src/bfs.cpp
+++ src/bfs.cpp
@@ -6,16 +6,19 @@
 
 namespace bfs {
 
-#define VISITED_BIT 30
-#define MARKED_BIT 31
-#define MARKED 1 << MARKED_BIT
-#define VISITED 0b11 << VISITED_BIT
-#define VISITED_MASK 0b01 << VISITED_BIT
-#define ALPHA 14
-#define BETA 24
+constexpr int ALPHA = 14;
+constexpr int BETA = 24;
+constexpr int MARKED_BIT = 31;
+constexpr int MARKED_MASK = 1 << MARKED_BIT;
+constexpr int VISITED_BIT = 30;
+constexpr int VISITED_MASK = 0b01 << VISITED_BIT;
+constexpr int MARKED_VISITED_MASK = 0b11 << VISITED_BIT;
 
 enum class Direction { TOP_DOWN, BOTTOM_UP };
 typedef std::vector<vidType> frontier;
+
+#define IS_VISITED(i) (((merged[i]) & VISITED_MASK) != 0)
+#define IS_MARKED(i) (((merged[i]) & MARKED_MASK) != 0)
 
 class Graph : public BaseGraph {
   eidType *rowptr;
@@ -23,37 +26,31 @@ class Graph : public BaseGraph {
   vidType *merged;
   uint32_t unexplored_edges;
   Direction dir;
-  bool *visited;
   [[maybe_unused]] uint64_t N;
   [[maybe_unused]] uint64_t M;
   uint32_t edges_frontier;
 
 public:
-  Graph(eidType *rowptr, vidType *col, vidType *merged, bool *visited, uint64_t N, uint64_t M)
-      : rowptr(rowptr), col(col), merged(merged), visited(visited), N(N), M(M) {
+  Graph(eidType *rowptr, vidType *col, vidType *merged, uint64_t N, uint64_t M)
+      : rowptr(rowptr), col(col), merged(merged), N(N), M(M) {
     unexplored_edges = M;
   }
   ~Graph() {}
 
-  inline vidType copy_unmarked(vidType i) { return merged[i] & ~(VISITED); }
-
-  inline bool is_marked(vidType i) { return ((merged[i]) & MARKED) != 0; }
-  // inline bool is_visited(vidType i) { return ((merged[i] & VISITED_MASK) >> VISITED_BIT) != 0; }
-  inline bool is_visited(vidType i) { return visited[i]; }
+  inline vidType copy_unmarked(vidType i) { return merged[i] & ~(MARKED_VISITED_MASK); }
 
   inline void set_distance(vidType i, weight_type distance) {
-    merged[i] = distance | VISITED;
-    visited[i] = true;
+    merged[i] = distance | MARKED_VISITED_MASK;
   }
 
-  inline bool is_unconnected(vidType i) { return (merged[i] & ~(VISITED)) == 0; }
+  inline bool is_unconnected(vidType i) { return (merged[i] & ~(MARKED_VISITED_MASK)) == 0; }
 
   inline bool is_leaf(vidType i) { return (rowptr[i] == rowptr[i + 1] - 1); }
 
   void compute_distances(weight_type *distances, vidType source) {
     #pragma omp parallel for schedule(auto)
     for (uint64_t i = 0; i < N; i++) {
-      if (is_visited(rowptr[i])) {
+      if (IS_VISITED(rowptr[i])) {
         distances[i] = copy_unmarked(rowptr[i]);
       }
     }
@@ -89,16 +86,19 @@ public:
     return to_switch;
   }
 
+  #pragma omp declare reduction(vec_add: std::vector<vidType>, std::vector<std::pair<vidType, bool>>: \
+    omp_out.insert(omp_out.end(), omp_in.begin(), omp_in.end()))
+
   void bottom_up_step(frontier this_frontier, frontier &next_frontier, weight_type distance) {
     // std::cout << "Bottom up step\n";
     #pragma omp parallel for reduction(vec_add: next_frontier) reduction(+: edges_frontier) schedule(auto)
     for (vidType i = 0; i < N; i++) {
       vidType start = rowptr[i];
-      if (is_visited(start) || is_unconnected(start)) {
+      if (IS_VISITED(start) || is_unconnected(start)) {
         continue;
       }
       for (vidType j = start + 1; j < rowptr[i+1]; j++) {
-        if (is_visited(merged[j]) && copy_unmarked(merged[j]) == distance - 1) {
+        if (IS_VISITED(merged[j]) && copy_unmarked(merged[j]) == distance - 1) {
           // If neighbor is in frontier, add this vertex to next frontier
           add_to_frontier(next_frontier, start);
           set_distance(start, distance);
@@ -108,16 +108,14 @@ public:
     }
   }
 
-  #pragma omp declare reduction(vec_add: std::vector<vidType>: \
-    omp_out.insert(omp_out.end(), omp_in.begin(), omp_in.end()))
-
   void top_down_step(frontier this_frontier, frontier &next_frontier, weight_type distance) {
     // std::cout << "Top down step\n";
+    // print merged
     #pragma omp parallel for reduction(vec_add: next_frontier) reduction(+: edges_frontier) schedule(auto)
     for (const auto &v : this_frontier) {
-      for (vidType i = v + 1; !is_marked(i) && (i < N + M); ++i) {
+      for (vidType i = v + 1; !IS_MARKED(i); i++) {
         vidType neighbor = merged[i];
-        if (!is_visited(neighbor)) {
+        if (!IS_VISITED(neighbor)) {
           add_to_frontier(next_frontier, neighbor);
           set_distance(neighbor, distance);
         }
@@ -134,6 +132,7 @@ public:
     weight_type distance = 1;
     while (!this_frontier.empty()) {
       // print_frontier(this_frontier);
+      // std::cout << "Edges in frontier " << edges_frontier << ", vertices in frontier " << this_frontier.size();
       frontier next_frontier;
       next_frontier.reserve(this_frontier.size());
       if (dir == Direction::TOP_DOWN) {
@@ -169,7 +168,7 @@ void merged_csr(eidType *rowptr, vidType *col, vidType *merged, uint64_t N, uint
   // Add degree of each vertex to the start of its neighbor list
   for (vidType i = 0; i < N; i++) {
     vidType start = rowptr[i];
-    merged[merged_index++] = get_degree(rowptr, i) | MARKED;
+    merged[merged_index++] = get_degree(rowptr, i) | MARKED_MASK;
     for (vidType j = start; j < rowptr[i + 1]; j++, merged_index++) {
       merged[merged_index] = rowptr[col[j]] + col[j];
     }
@@ -179,15 +178,14 @@ void merged_csr(eidType *rowptr, vidType *col, vidType *merged, uint64_t N, uint
   for (vidType i = 0; i <= N; i++) {
     rowptr[i] = rowptr[i] + i;
   }
+  merged[M+N] = MARKED_MASK;
 }
 
 BaseGraph *initialize_graph(eidType *rowptr, vidType *col, uint64_t N,
                             uint64_t M) {
-  vidType *merged = new vidType[M+N];
+  vidType *merged = new vidType[M+N+1];
   merged_csr(rowptr, col, merged, N, M);
-  bool *visited = new bool[N+M];
-  std::fill(visited, visited + N + M, false);
-  return new Graph(rowptr, col, merged, visited, N, M);
+  return new Graph(rowptr, col, merged, N, M);
 }
 
 } // namespace bfs
