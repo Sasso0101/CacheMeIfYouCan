581e3e134d0190c3147bb547e1b34dcbe4b0bae5
sasso0101 <git@sasso.dev>
2025-01-20 19:21:19 +0100

    Fix

--- scripts/download_datasets.sh
+++ scripts/download_datasets.sh
@@ -5,6 +5,6 @@ wget -nc "https://speedcode-proto-problem-data.s3.us-east-1.amazonaws.com/ppopp-
 wget -nc "https://speedcode-proto-problem-data.s3.us-east-1.amazonaws.com/ppopp-graphs-v3/enwiki-2023_sym.pbin"
 wget -nc "https://speedcode-proto-problem-data.s3.us-east-1.amazonaws.com/ppopp-graphs-v3/north-america_wgh.pbin"
 wget -nc "https://speedcode-proto-problem-data.s3.us-east-1.amazonaws.com/ppopp-graphs-v3/GeoLifeNoScale_5_sym_wgh.pbin"
-wget -nc "https://speedcode-proto-problem-data.s3.us-east-1.amazonaws.com/ppopp-graphs-v3/hollywood_200<c9_sym.pbin"
+wget -nc "https://speedcode-proto-problem-data.s3.us-east-1.amazonaws.com/ppopp-graphs-v3/hollywood_2009_sym.pbin"
 wget -nc "https://speedcode-proto-problem-data.s3.us-east-1.amazonaws.com/ppopp-graphs-v3/soc-LiveJournal1_sym.pbin"
 wget -nc "https://speedcode-proto-problem-data.s3.us-east-1.amazonaws.com/ppopp-graphs-v3/south-america_wgh.pbin"
